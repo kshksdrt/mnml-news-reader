d5971b0d7be8f9a99424c3d1b0b540bc6dc60a8e
kshksdrt <34638773+kshksdrt@users.noreply.github.com>
2020-07-23 10:53:41 +0200

    Project setup

---
+++ src/components/News.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function News(url) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react"
+
+function getSavedValue (key, initialValue) {
+  let savedValue
+  if (localStorage.getItem(key) !== "undefined") savedValue = JSON.parse(localStorage.getItem(key))
+  if (savedValue) return savedValue
+
+  if (typeof initialValue == 'function') return initialValue()
+  return initialValue
+}
+
+export default function useLocalStorage (key, initialValue) {
+  const [value, setValue] = useState(() => {
+    return getSavedValue(key, initialValue)
+  })
+
+  useEffect(() => {
+    console.log(value)
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [value])
+
+  return [value, setValue]
+}
